e88af2c16d719e998afdd74c3501caf236fe6cf1
Thomas Schmucker <ts@its1.de>
2023-12-15 09:56:48 +0100

    Lösung für Tag 15, Teil 2

--- src/day15.cpp
+++ src/day15.cpp
@@ -1,5 +1,7 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -46,11 +48,52 @@ calculate_hash(string_view str)
 void
 part1()
 {
-	const auto line  = read_file("data/day15.txt");
-	const auto parts = split(line, ',');
+	const auto    line  = read_file("data/day15.txt");
+	const auto    parts = split(line, ',');
 	unsigned long value = 0;
 	for ( const auto& part: parts ) {
 		value += calculate_hash(part);
+	}
+	cout << value << endl;
+}
+
+void
+part2()
+{
+	vector<list<tuple<string, long>>> boxes(256);
+
+	const auto line  = read_file("data/day15.txt");
+	const auto parts = split(line, ',');
+	for ( const auto& part: parts ) {
+		const auto pos  = part.find_first_of("=-");
+		const auto lens = part.substr(0, pos);
+		auto&      box  = boxes[calculate_hash(lens)];
+
+		if ( part[pos] == '=' ) {
+			const auto value = stol(part.substr(pos + 1));
+
+			auto iter = find_if(box.begin(), box.end(), [&lens](const auto& element) { return get<0>(element) == lens; });
+			if ( iter == box.end() ) {
+				box.emplace_back(lens, value);
+			}
+			else {
+				*iter = { lens, value };
+			}
+		}
+		else {
+			box.remove_if([&lens](const auto& element) { return get<0>(element) == lens; });
+		}
+	}
+
+	long value = 0;
+	for ( size_t idx = 0; idx != boxes.size(); ++idx ) {
+		const auto& box = boxes[idx];
+
+		long lens_number = 1;
+		for ( const auto& lens: box ) {
+			value += static_cast<long>(idx + 1) * lens_number * get<1>(lens);
+			++lens_number;
+		}
 	}
 	cout << value << endl;
 }
@@ -59,4 +102,5 @@ int
 main()
 {
 	part1();
+	part2();
 }
